b9d9acb47af84127579ceaaaa12cebb3650e4c87
minojeyam <maryannminerva@gmail.com>
2024-04-07 12:39:35 +0530

    Updated List

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,25 @@ export default function App () {
   function handleSubmit (e) {
     e.preventDefault()
 
-    setToDos([
-      ...todos,
+    setToDos( (currentTodos) => {
+      return [
+      ...currentTodos,
       { id: crypto.randomUUID(), title: newItem, completed:false},
-    ])
+      ]
+    })
+    setNewItem("")
   }
-  console.log(todos);
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
 
   return (
     <>
@@ -34,20 +47,22 @@ export default function App () {
       </form>
       <h1 className="header">To Do List</h1>
       <ul className="list">
-        <li>
-          <label> 
-            <input type="checkbox"/>
-            Item 1 
-          </label>
-          <button className="btn btn-danger"> Delete </button>
-        </li>
-        <li>
-          <label> 
-            <input type="checkbox"/>
-            Item 2 
-          </label>
-          <button className="btn btn-danger"> Delete </button>
-        </li>
+        {todos.map( todo => {
+          return (
+            <li key={todo.id}>
+              <label> 
+                <input 
+                  type="checkbox" 
+                  checked={todo.completed} 
+                  onChange={e => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title} 
+              </label>
+              <button className="btn btn-danger"> Delete </button>
+            </li>
+          )
+         }
+        )}
       </ul>
     </>
   )
